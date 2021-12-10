4c1223183238aae7eb338619de9c5c1b0afb8775
Conan Leong <conanaleong@gmail.com>
2021-12-09 17:39:41 -0800

    v1.0

--- calc.js
+++ calc.js
@@ -5,6 +5,7 @@ var num1 = ''
 var num2 = ''
 var operator = ''
 var whichNum = 'num1'
+var debugging = false
 
 function clearDisplay() {
     whichNum = 'num1'
@@ -39,26 +40,42 @@ function number(num) {
 }
 
 function point() {
-    alert('This doesn\'t work yet')
-    //check for existing point
+    // 
+
+    if (num1 == result && operator == '') {
+        num1 = 0
+    }
+
+    if (whichNum == 'num1') {
+        if (String(num1).split().includes('.')) {      // doesn't work
+            } else {
+            num1 += '' + '.' 
+            }
+    } else {
+        if (String(num2).split().includes('.')) {            
+            } else {
+            num2 += '' + '.' 
+            }
+    }
+    updateDisplay()
 
 }
 
 function equals() {
     if (operator == '+') {
-        result = parseInt(num1) + parseInt(num2)
+        result = parseFloat(num1) + parseFloat(num2)
         clearDisplay()
         num1 = result
-    } else if (operator == '-') {
-        result = parseInt(num1) - parseInt(num2)
+    } else if (operator == '−') {
+        result = parseFloat(num1) - parseFloat(num2)
         clearDisplay()
         num1 = result
-    } else if (operator == '*') {
-        result = parseInt(num1) * parseInt(num2)
+    } else if (operator == '×') {
+        result = parseFloat(num1) * parseFloat(num2)
         clearDisplay()
         num1 = result
-    } else if (operator == '/') {
-        result = parseInt(num1) / parseInt(num2)
+    } else if (operator == '÷') {
+        result = parseFloat(num1) / parseFloat(num2)
         clearDisplay()
         num1 = result
     }
@@ -70,18 +87,37 @@ function equals() {
 
 function updateDisplay() {
     // fix too many digits
-    display.innerHTML = parseInt(num1) + operator
+    display.innerHTML = Math.round(parseFloat(num1) * 100) / 100 + operator
     if (whichNum == 'num2' && num2 != '') {
-        display.innerHTML += parseInt(num2)
+        display.innerHTML += Math.round(parseFloat(num2) * 100) / 100
     }
 
 
     //debugging
+
+    if (debugging == true) {
     document.getElementById('num1').innerHTML = "num1: " + num1
     document.getElementById('num2').innerHTML = "num2: " + num2
     document.getElementById('operator').innerHTML = "operator: " + operator
     document.getElementById('whichNum').innerHTML = "whichNum: " + whichNum
     document.getElementById('result').innerHTML = "result: " + result
+    } else {
+        document.getElementById('num1').innerHTML = ''
+        document.getElementById('num2').innerHTML = ''
+        document.getElementById('operator').innerHTML = ''
+        document.getElementById('whichNum').innerHTML = ''
+        document.getElementById('result').innerHTML = ''
+    }
+}
+
+function debugToggle() {
+    if (debugging == true) {
+        debugging = false
+        } else {
+        debugging = true
+        } 
+
+    updateDisplay()
 }
 
 number('0')
