8e831e39e3d750f9f9b84503fcac826d97f538bd
Conan Leong <conanaleong@gmail.com>
2021-12-09 16:47:45 -0800

    v0.6 - all operations

--- calc.js
+++ calc.js
@@ -1,18 +1,16 @@
 
 const display = document.getElementById('display');
-var calculation = 0
 var result = undefined
-var num1 = 0
-var num2 = 0
+var num1 = ''
+var num2 = ''
 var operator = ''
 var whichNum = 'num1'
 
 function clearDisplay() {
-    calculation = 0
     whichNum = 'num1'
     operator = ''
-    num1 = 0
-    num2 = 0
+    num1 = ''
+    num2 = ''
     number('0')
 }
 
@@ -21,7 +19,6 @@ function setOperator(op) {
         equals()
     }
 
-    calculation += '' + op
     operator = op
     whichNum = 'num2'
     updateDisplay()
@@ -32,35 +29,37 @@ function number(num) {
     if (num1 == result && operator == '') {
         num1 = 0
     }
-    
-    if (calculation == 0) {
-        calculation = ''
-    }
 
     if (whichNum == 'num1') {
         num1 += '' + num
     } else {
         num2 += '' + num
     }
-
-    calculation += '' + num
     updateDisplay()
 }
 
 function point() {
-    if (['.'].includes(calculation.slice(-1))) {
-    } else {
-        calculation += '' + '.'
-        updateDisplay()
-    }
+    alert('This doesn\'t work yet')
+    //check for existing point
+
 }
 
 function equals() {
     if (operator == '+') {
-        calculation = parseInt(num1) + parseInt(num2)
-        result = calculation
+        result = parseInt(num1) + parseInt(num2)
         clearDisplay()
-        calculation = result
+        num1 = result
+    } else if (operator == '-') {
+        result = parseInt(num1) - parseInt(num2)
+        clearDisplay()
+        num1 = result
+    } else if (operator == '*') {
+        result = parseInt(num1) * parseInt(num2)
+        clearDisplay()
+        num1 = result
+    } else if (operator == '/') {
+        result = parseInt(num1) / parseInt(num2)
+        clearDisplay()
         num1 = result
     }
 
@@ -71,11 +70,13 @@ function equals() {
 
 function updateDisplay() {
     // fix too many digits
-    display.innerHTML = num1 + operator + num2
+    display.innerHTML = parseInt(num1) + operator
+    if (whichNum == 'num2' && num2 != '') {
+        display.innerHTML += parseInt(num2)
+    }
 
 
     //debugging
-    document.getElementById('calculation').innerHTML = "calculation: " + calculation
     document.getElementById('num1').innerHTML = "num1: " + num1
     document.getElementById('num2').innerHTML = "num2: " + num2
     document.getElementById('operator').innerHTML = "operator: " + operator
